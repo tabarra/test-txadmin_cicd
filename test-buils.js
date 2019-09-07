9e2c0862c79bebd58fb76d9b277e80a1f1af237b
Andre Tabarra <tabarra@vexguard.net>
2019-09-07 06:47:51 -0300

    xcxcvxvc

--- test-buils.js
+++ test-buils.js
@@ -3,3 +3,4 @@ let multiplier = parseInt(1024+(1024**2)*Math.random());
 let randomCrap = "lets just pretend this is the result of some pkg script or something :) ".repeat(multiplier)
 fs.writeFileSync('dist/out.exe', randomCrap);
 console.log(`File created with size ${randomCrap.length}`);
+//random edit
