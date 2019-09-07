1c234fe8bb863c49f30652416f8f046f80fa0c4f
Andre Tabarra <tabarra@vexguard.net>
2019-09-07 06:25:23 -0300

    test name

---
+++ release-name.js
@@ -0,0 +1,1 @@
+console.log("RandomShit " + Date.now())
--- test-buils.js
+++ test-buils.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
 let multiplier = parseInt(1024+(1024**2)*Math.random());
 let randomCrap = "lets just pretend this is the result of some pkg script or something :) ".repeat(multiplier)
-fs.writeFileSync('dist/out.bin', randomCrap);
+fs.writeFileSync('dist/out.exe', randomCrap);
 console.log(`File created with size ${randomCrap.length}`);
